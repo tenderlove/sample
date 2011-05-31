4bebbd8a9535da1335d24043840f9182442b9030
Aaron Patterson <aaron.patterson@gmail.com>
2011-05-31 13:59:07 -0700

    basic application that returns "hello world"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
   def index
-    @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-    end
+    render :text => 'hello world'
   end
 
   # GET /users/1
