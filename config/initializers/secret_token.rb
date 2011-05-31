d0ce5d095eaf090dab880001885a6e5b68e54b6e
Aaron Patterson <aaron.patterson@gmail.com>
2011-05-31 11:56:01 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leaky::Application.config.secret_token = 'f087a9adbc86e72a10019c752ca056bc2dee3407bc50ac1c02be7acb8d4af5ecbe9ae5f5dc2cc756691d269697d14627ff5a58c62b16c89ad86a0f5d51315f85'
